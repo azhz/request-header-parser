8ba1e6305ad50fe9e5fb55d7a9682027d4065fb4
Ajinkya <ajinkya233@gmail.com>
2017-03-21 15:29:55 +0000

    Added log messages

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ var app = express();
 app.use(express.static('client'));
 
 app.get('/api/whoami', function(req, res) {
+    console.log('Received a request.');
     var clientData = {
         ip : req.ip,
         language : req.get("accept-language").split(',')[0],
         software : req.get("user-agent").split(')')[0].split('(')[1]
     }
     res.end(JSON.stringify(clientData));
+});
+
+app.get('/api/whoami2', function(req, res) {
+    console.log('Received a request 2.');
+    res.end('link 2');
 });
 
 app.listen(8080);
